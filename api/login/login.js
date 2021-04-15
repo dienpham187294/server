42d432f97537e1c19427ea45ccc7be4b50dd5a77
dienpham187294 <dienpham187294@gmail.com>
2021-04-15 14:55:28 +0700

    make it better

--- api/login/login.js
+++ api/login/login.js
@@ -8,30 +8,47 @@ function getInfo(fs, io, data) {
         io.to(data[2]).emit("get_info_One", jsonFile)
 
     })
-
-    fs.readFile(`./database/record/${data[1]}Correct.txt`, 'utf8', (err, jsonFile) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        io.to(data[2]).emit("get_info_OneCorrect", jsonFile)
-
-    })
-    fs.readFile(`./database/record/${data[1]}unCorrect.txt`, 'utf8', (err, jsonFile) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        io.to(data[2]).emit("get_info_OneunCorrect", jsonFile)
-
-    })
 };
 
-
-
+function getinfodangkytaikhoan(fs, io, data) {
+    fs.readFile('./database/user.txt', 'utf8', (err, jsonFile) => {
+        if (err) {
+            return
+        }
+        io.to(data[1]).emit("getinfodangkytaikhoan", JSON.parse(jsonFile))
+    })
+}
+function dangkytaikhoan(fs, io, data) {
+    fs.readFile('./database/user.txt', 'utf8', (err, jsonFile) => {
+        if (err) {
+            return
+        }
+        let arrOfUser = JSON.parse(jsonFile);
+        arrOfUser.push({
+            "username": data[2],
+            "password": data[5],
+            "name": data[3],
+            "position": "student",
+            "ipaddress": data[6],
+            "email": data[4],
+            "time": Date.now() + 5 * 24 * 60 * 60,
+            "status": "trial5ngay"
+        })
+        fs.writeFile('./database/user.txt', JSON.stringify(arrOfUser), (err) => {
+            if (err) throw err;
+        });
+    })
+}
 function allListen(fs, io, data) {
     if (data[0] === "get_info_One") {
         getInfo(fs, io, data);
     }
+    if (data[0] === "dangkytaikhoan") {
+        getinfodangkytaikhoan(fs, io, data);
+    }
+    if (data[0] === "DangkyInfoSubmit") {
+        dangkytaikhoan(fs, io, data);
+    }
+
 }
 module.exports = { allListen }
--- index.js
+++ index.js
@@ -6,31 +6,22 @@ const router = require('./router');
 const app = express();
 const fs = require('file-system');
 const fnAdmin = require("./api/admin/admin");
-const fnMonopoly = require("./api/monopoly/monopoly");
 const fnChess = require("./api/chess/chess");
-const fnRead = require("./api/read/read");
 const fnSaveFile = require("./api/saveFile/saveFile");
 const fnLogin = require("./api/login/login");
 app.use(cors());
 app.use(router);
 
 io.on('connection', client => {
-    console.log("connected : " + client.id);
+    // console.log("connected : " + client.id);
     //Amin -----------------------------------
     client.on("admin", (data) => {
         fnAdmin.allListen(fs, io, data);
     })
-    //Monopoly -----------------------------------
-    client.on("monopoly", (data) => {
-        fnMonopoly.allListen(fs, io, data);
-    })
     client.on("chess", (data) => {
         fnChess.allListen(fs, io, data);
     })
-    //READINNG-----------------------------------
-    client.on("read", (data) => {
-        fnRead.allListen(fs, io, data);
-    })
+    
     client.on("saveFile", (data) => {
         fnSaveFile.allListen(fs, io, data);
     })
@@ -41,9 +32,7 @@ io.on('connection', client => {
     //All -----------------------------------
     //Update Disconnect
     client.on('disconnect', () => {
-        console.log("disconnect : " + client.id);
-        fnMonopoly.disConnect(fs, io, client.id);
-        fnRead.disConnect(fs, io, client.id);
+        fnChess.disConnect(fs, io, client.id);
     });
 });
 
