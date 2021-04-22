3a45537299b429761882662f8644c7ddce0b2b89
dienpham187294 <dienpham187294@gmail.com>
2021-04-22 15:29:29 +0700

    make it better

--- api/admin/admin.js
+++ api/admin/admin.js
@@ -5,6 +5,38 @@ function adminGetInfo(fs, io, data) {
         }
         io.to(data[1]).emit("adminInfo", JSON.parse(jsonFile))
     })
+    fs.readFile('./database/LophocFree.txt', 'utf8', (err, jsonFile) => {
+        if (err) {
+            return
+        }
+        io.to(data[1]).emit("adminInfoLichHoc", JSON.parse(jsonFile))
+    })
+}
+function themlophoc(fs, io, data) {
+    fs.readFile('./database/LophocFree.txt', 'utf8', (err, jsonFile) => {
+        if (err) {
+            return
+        }
+        let arrTemp = JSON.parse(jsonFile);
+        arrTemp.push({
+            "name": data[1],
+            "time": data[2],
+            "status": data[3],
+            "members": []
+        })
+        fs.writeFile('./database/LophocFree.txt', JSON.stringify(arrTemp));
+        io.to(data[4]).emit("adminInfoLichHoc", arrTemp)
+
+
+    })
+
+}
+
+function doitrangthai(fs, io, data) {
+    if (data[1].length > 1) {
+        fs.writeFile('./database/LophocFree.txt', JSON.stringify(data[1]));
+        io.to(data[2]).emit("adminInfoLichHoc", data[1])
+    }
 }
 function allListen(fs, io, data) {
     if (data[0] === "get_info") {
@@ -17,6 +49,12 @@ function allListen(fs, io, data) {
         } else {
             io.to(data[2]).emit("adminInfoError", "Not a Json File");
         }
+    }
+    if (data[0] === "themlophoc") {
+        themlophoc(fs, io, data)
+    }
+    if (data[0] === "doitrangthai") {
+        doitrangthai(fs, io, data)
     }
 }
 
--- api/login/login.js
+++ api/login/login.js
@@ -30,6 +30,7 @@ function dangkytaikhoan(fs, io, data) {
             "name": data[2],
             "position": "student",
             "ipaddress": data[5],
+            "phone": data[6],
             "email": data[3],
             "time": Date.now() + 5 * 24 * 60 * 60 * 1000,
             "status": "Trial5ngay"
@@ -39,6 +40,46 @@ function dangkytaikhoan(fs, io, data) {
                 if (err) throw err;
             });
         }
+    })
+}
+function DangkyLichHocInfo(fs, io, data) {
+    fs.readFile('./database/LophocFree.txt', 'utf8', (err, jsonFile) => {
+        if (err) {
+            return
+        }
+        io.to(data[1]).emit("DangkyLichHocInfo", JSON.parse(jsonFile));
+    })
+}
+function DangKyLopHoc_Pickclass(fs, io, data) {
+    fs.readFile('./database/LophocFree.txt', 'utf8', (err, jsonFile) => {
+        if (err) {
+            return
+        }
+        let arrTemp = JSON.parse(jsonFile);
+        arrTemp.forEach(e => {
+            if (e.members.length !== 0) {
+                e.members.forEach(e => {
+                    if (e.name === data[2]) {
+                        e.status = false;
+                    }
+                })
+            }
+        });
+        arrTemp.forEach(e => {
+            if (e.name === data[1]) {
+                e.members.push({ "name": data[2], "status": true })
+            }
+
+        });
+
+
+        if (JSON.stringify(arrTemp).length > 10) {
+            fs.writeFile('./database/LophocFree.txt', JSON.stringify(arrTemp), (err) => {
+                if (err) throw err;
+            });
+        }
+        io.to(data[3]).emit("DangKyLopHoc_Pickclass", arrTemp);
+        // // // io.to(data[3]).emit("DangkyLichHocInfo", arrTemp);
     })
 }
 function allListen(fs, io, data) {
@@ -51,7 +92,12 @@ function allListen(fs, io, data) {
     if (data[0] === "DangkyInfoSubmit") {
         dangkytaikhoan(fs, io, data);
     }
-
+    if (data[0] === "DangkyLichHocInfo") {
+        DangkyLichHocInfo(fs, io, data);
+    }
+    if (data[0] === "DangKyLopHoc_Pickclass") {
+        DangKyLopHoc_Pickclass(fs, io, data);
+    }
 }
 module.exports = { allListen }
 
